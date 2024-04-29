1ebda6a624335c52fbb54a487d46c1369cff9513
harristheharris <anthonyharris7777@gmail.com>
2024-04-29 12:54:09 -0400

    idek, trying ot deploy this to heroku

--- server.js
+++ server.js
@@ -12,11 +12,18 @@ const api = require('./public/assets/js/index.js');
 const { clog } = require('./middleware/clog');
 const { requestTime } = require('./middleware/requestTime');
 const { log } = require('console');
+
 //setting our our port to 3001
 //the process.env is an object that contains all of the environment vars that are currently set for our process
 // we add the '||' to specify that if no environmental vars are set, then the default is 3001
 // set environmental var OR 3001
-const PORT = process.env.PORT || 3001;
+//heroku stuff
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 //setting express to a var
 const app = express();
 //the use() function is used to mount the middleware functions at a specified path
@@ -37,24 +44,65 @@ app.use('/api' , api);
 
 app.use(express.static('public'));
 
+// GET:api route to get the notes
 app.get('/api/notes', (req, res) => {
-    res.json(theNotes.slice())
+    res.json(theNotes.slice(1))
 })
 
-//GET route for the homepage
+//GET:html route for the homepage
 app.get('/', (req , res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-//GET route for notes
-
+//GET:html route for notes
 app.get('/notes' , (req , res) => {
     res.sendFile(path.join(__dirname , '/public/notes.html'))
+})
 
+//GET:html wild card route. a defualt
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+//the function that creates the new notes submitted by the user
+function createAnotherNote(reqBody, notesArray){
+    const aNote = reqBody;
+
+    //if aNote is NOT and array then make it one
+    if(!Array.isArray(notesArray)) {
+        aNote = []; 
+    }
+
+    //if our theNotes (which we are passing to the function) has length of zero then we push and empty array to it
+    if(notesArray.length === 0) {
+        notesArray.push(0);
+    }
+
+    body.id = notesArray[0];
+    notesArray[0]++
+
+    notesArray.push(aNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray)
+    );
+
+    return aNote;
+
+
+    
+}
+
+
+//POST:api create a post to post the new note submitted
+app.post('/api/notes' , (req, res) => {
+    const newNote = createAnotherNote(req.body, theNotes)
+    res.json(newNote)
 })
 
 
-app.listen(PORT , () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port , () => {
+    console.log(`Server running on http://localhost:${port}`)
 })
 
